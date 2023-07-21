65bef18c872c5c639772b096b66219f1d1e99ac5
Tomer Iwan <iwantomer@hotmail.com>
2023-07-21 13:20:17 +0200

    feat: show data in the app

--- R/server.R
+++ R/server.R
@@ -2,20 +2,52 @@
 #'
 #' This file contains the server logic for the Shiny app.
 #'
+#' @importFrom DT datatable
 #' @export
 app_server <- function(input, output, session) {
-  output$distPlot <- shiny::renderPlot({
+  data <- reactive({
     req(input$file)
 
+    # Get the full path of the uploaded file
+    full_path <- normalizePath(input$file$datapath)
+
+    # Print the file path
+    print(full_path)
+
     # Read the uploaded file
-    data <- read.csv(input$file$datapath)
+    read.csv(full_path)
+  })
 
-    # Display the dataframe in the app
-    View(data)
+  # Get the column names of the dataframe and ensure they are unique
+  column_names <- reactive({
+    req(data())
+    colnames(data())
+  })
 
-    # # Draw the histogram
-    # hist(data, col = 'darkgray', border = 'white',
-    #      xlab = 'Waiting time to next eruption (in mins)',
-    #      main = 'Histogram of waiting times')
+  # Remove the selected column from the second dropdown
+  observe({
+    req(input$col_select_1)
+    updateSelectInput(session, "col_select_2", choices = setdiff(column_names(), input$col_select_1))
+  })
+
+  # Display an input field when the second dropdown has a "NULL" option
+  output$mean_input <- renderUI({
+    if (is.null(input$col_select_2)) {
+      shiny::numericInput("mean_val", "Enter Mean:", value = 0, step = 0.1)
+    }
+  })
+
+  output$dataTable <- DT::renderDataTable({
+    req(data())
+
+    # Display the dataframe
+    DT::datatable(data())
+  })
+
+  output$dataView <- renderPrint({
+    req(data())
+
+    # Return the column names of the uploaded dataframe
+    colnames(data())
   })
 }
--- R/ui.R
+++ R/ui.R
@@ -11,12 +11,17 @@ app_ui <- function() {
     # Sidebar with a browse button for file upload
     shiny::sidebarLayout(
       shiny::sidebarPanel(
-        shiny::fileInput("file", "Upload a file")
+        shiny::fileInput("file", "Upload a file"),
+        shiny::selectInput("col_select_1", "Select Column 1", NULL),
+        shiny::selectInput("col_select_2", "Select Column 2", NULL),
+        shiny::uiOutput("mean_input")  # Display the input field here
       ),
 
       # Show a plot of the uploaded data
       shiny::mainPanel(
-        shiny::plotOutput("distPlot")
+        shiny::plotOutput("distPlot"),
+        shiny::verbatimTextOutput("dataView"),  # Dedicated "View" pane
+        DT::dataTableOutput("dataTable")
       )
     )
   )
