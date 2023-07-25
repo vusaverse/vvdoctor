1f9e393d49b61e4205f03df4403d95390e2955e7
Tomer Iwan <iwantomer@hotmail.com>
2023-07-25 10:51:53 +0200

    fix: simplify app to only view datatable

--- R/server.R
+++ R/server.R
@@ -1,101 +1,113 @@
-#' Title
-#'
-#'
+# Shiny App Server
 #' @export
-#'
 app_server <- function(input, output, session) {
   data <- handle_file_upload(input, output, session)
 
-  # Reactive value to track whether a file has been uploaded
-  observe({
+  # Display the uploaded data as a datatable
+  output$dataTable <- DT::renderDataTable({
     req(data())
-    # Set fileUploaded to TRUE to show the dependent and independent variable dropdowns
-    updateNumericInput(session, "fileUploaded", value = 1)
-  })
-
-  # Display an input field when the second dropdown has a "NULL" option
-  output$mean_input <- renderUI({
-    req(input$dependent_var)
-    if (is.null(input$independent_var) || input$independent_var == "") {
-      shiny::numericInput("mean_val", "Enter Mean:", value = 0, step = 0.1)
-    }
-  })
-
-  # Display the variable class info using a separate function
-  output$variable_class_info <- renderText({
-    req(input$independent_var)
-    var_class_info <- get_variable_class_info(data()[[input$independent_var]])
-    var_class_info
-  })
-
-  # Display the recommended statistical test based on the chosen variables
-  output$recommended_test <- renderText({
-    recommended_test <- choose_statistical_test(input$independent_var, input$dependent_var)
-    paste("Recommended Statistical Test:", recommended_test)
-  })
-
-  # Plot histogram for continuous and normally distributed variable
-  output$distPlot <- renderPlot({
-    req(input$independent_var, data())
-
-    # Get the variable class info
-    var_class_info <- get_variable_class_info(data()[[input$independent_var]])
-
-    if (is.numeric(data()[[input$independent_var]]) &&
-        grepl("Continuous (Normally Distributed)", var_class_info[1])) {
-      hist(data()[[input$independent_var]],
-           main = "Histogram of Independent Variable",
-           xlab = "Independent Variable",
-           col = "darkgray")
-    } else {
-      NULL
-    }
-  })
-
-  # Generate histogram for continuous dependent variable
-  output$histPlot <- renderPlot({
-    req(input$independent_var, input$dependent_var, data())
-
-    if (input$dependent_var == "1" && input$independent_var == "continuous" &&
-        is.numeric(data()[[input$independent_var]])) {
-      hist(data()[[input$independent_var]],
-           main = "Histogram of Continuous Dependent Variable",
-           xlab = "Dependent Variable",
-           col = "darkgray")
-    } else {
-      NULL
-    }
-  })
-
-  # Display the uploaded data as a data table
-  output$dataTable <- renderDataTable({
-    req(data())
-
-    # Display the dataframe using the helper function
-    display_data_table(data())
-  })
-
-  # Display information about the dependent variable
-  output$dependent_var_info <- renderText({
-    req(input$dependent_var, data())
-
-    dependent_var <- data()[[input$dependent_var]]
-
-    if (is.numeric(dependent_var)) {
-      # Check for normality of continuous dependent variable
-      var_class_info <- get_variable_class_info(dependent_var)
-      return(var_class_info)
-    } else {
-      # Check if the categorical variable is binary or nominal
-      unique_values <- unique(dependent_var)
-      if (length(unique_values) == 2) {
-        return("Dependent Variable: Categorical (Binary)")
-      } else {
-        return("Dependent Variable: Categorical (Nominal)")
-      }
-    }
+    DT::datatable(data())
   })
 }
 
 
-
+#' #' Title
+#' #'
+#' #'
+#' #'
+#' app_server <- function(input, output, session) {
+#'   data <- handle_file_upload(input, output, session)
+#'
+#'   # Reactive value to track whether a file has been uploaded
+#'   observe({
+#'     req(data())
+#'     # Set fileUploaded to TRUE to show the dependent and independent variable dropdowns
+#'     updateNumericInput(session, "fileUploaded", value = 1)
+#'   })
+#'
+#'   # Display an input field when the second dropdown has a "NULL" option
+#'   output$mean_input <- renderUI({
+#'     req(input$dependent_var)
+#'     if (is.null(input$independent_var) || input$independent_var == "") {
+#'       shiny::numericInput("mean_val", "Enter Mean:", value = 0, step = 0.1)
+#'     }
+#'   })
+#'
+#'   # Display the variable class info using a separate function
+#'   output$variable_class_info <- renderText({
+#'     req(input$independent_var)
+#'     var_class_info <- get_variable_class_info(data()[[input$independent_var]])
+#'     var_class_info
+#'   })
+#'
+#'   # Display the recommended statistical test based on the chosen variables
+#'   output$recommended_test <- renderText({
+#'     recommended_test <- choose_statistical_test(input$independent_var, input$dependent_var)
+#'     paste("Recommended Statistical Test:", recommended_test)
+#'   })
+#'
+#'   # Plot histogram for continuous and normally distributed variable
+#'   output$distPlot <- renderPlot({
+#'     req(input$independent_var, data())
+#'
+#'     # Get the variable class info
+#'     var_class_info <- get_variable_class_info(data()[[input$independent_var]])
+#'
+#'     if (is.numeric(data()[[input$independent_var]]) &&
+#'         grepl("Continuous (Normally Distributed)", var_class_info[1])) {
+#'       hist(data()[[input$independent_var]],
+#'            main = "Histogram of Independent Variable",
+#'            xlab = "Independent Variable",
+#'            col = "darkgray")
+#'     } else {
+#'       NULL
+#'     }
+#'   })
+#'
+#'   # Generate histogram for continuous dependent variable
+#'   output$histPlot <- renderPlot({
+#'     req(input$independent_var, input$dependent_var, data())
+#'
+#'     if (input$dependent_var == "1" && input$independent_var == "continuous" &&
+#'         is.numeric(data()[[input$independent_var]])) {
+#'       hist(data()[[input$independent_var]],
+#'            main = "Histogram of Continuous Dependent Variable",
+#'            xlab = "Dependent Variable",
+#'            col = "darkgray")
+#'     } else {
+#'       NULL
+#'     }
+#'   })
+#'
+#'   # Display the uploaded data as a data table
+#'   output$dataTable <- renderDataTable({
+#'     req(data())
+#'
+#'     # Display the dataframe using the helper function
+#'     display_data_table(data())
+#'   })
+#'
+#'   # Display information about the dependent variable
+#'   output$dependent_var_info <- renderText({
+#'     req(input$dependent_var, data())
+#'
+#'     dependent_var <- data()[[input$dependent_var]]
+#'
+#'     if (is.numeric(dependent_var)) {
+#'       # Check for normality of continuous dependent variable
+#'       var_class_info <- get_variable_class_info(dependent_var)
+#'       return(var_class_info)
+#'     } else {
+#'       # Check if the categorical variable is binary or nominal
+#'       unique_values <- unique(dependent_var)
+#'       if (length(unique_values) == 2) {
+#'         return("Dependent Variable: Categorical (Binary)")
+#'       } else {
+#'         return("Dependent Variable: Categorical (Nominal)")
+#'       }
+#'     }
+#'   })
+#' }
+#'
+#'
+#'
--- R/ui.R
+++ R/ui.R
@@ -1,3 +1,48 @@
+#' # Shiny App UI
+#' app_ui <- function() {
+#'   shiny::fluidPage(
+#'     # Application title
+#'     shiny::titlePanel("Statistical Test App"),
+#'
+#'     # Use the shinyjs library
+#'     shinyjs::useShinyjs(),
+#'
+#'     # Sidebar with a browse button for file upload
+#'     shiny::sidebarLayout(
+#'       shiny::sidebarPanel(
+#'         shiny::fileInput("file", "Upload a file"),
+#'         shiny::selectInput("dependent_var", "Choose Dependent Variable",
+#'                            choices = NULL),
+#'         shiny::selectInput("independent_var", "Choose Independent Variable",
+#'                            choices = c("None", "continuous", "categorical")),
+#'         shiny::numericInput("mean_input", "Enter Mean:", value = 0, step = 0.1,
+#'                             min = -999, max = 999)
+#'       ),
+#'
+#'       # Show a plot of the uploaded data (Histogram for continuous variables)
+#'       shiny::mainPanel(
+#'         # Place the table and the rest of the elements in a fluidRow
+#'         shiny::fluidRow(
+#'           # Place the table in a column that takes up 6 out of 12 units
+#'           shiny::column(width = 6, DT::dataTableOutput("dataTable")),
+#'           # Place the histogram plot and other elements in a column that takes up 6 out of 12 units
+#'           shiny::column(width = 6,
+#'                         # Display the histogram only when a continuous variable is selected
+#'                         shiny::conditionalPanel(
+#'                           condition = "input.independent_var == 'continuous'",
+#'                           shiny::plotOutput("histPlot")
+#'                         ),
+#'                         shiny::verbatimTextOutput("dataView"),  # Display data info
+#'                         shiny::verbatimTextOutput("dependent_var_info")  # Display dependent variable info
+#'           )
+#'         )
+#'       )
+#'     )
+#'   )
+#' }
+
+
+
 # Shiny App UI
 #' @export
 app_ui <- function() {
@@ -11,31 +56,13 @@ app_ui <- function() {
     # Sidebar with a browse button for file upload
     shiny::sidebarLayout(
       shiny::sidebarPanel(
-        shiny::fileInput("file", "Upload a file"),
-        shiny::selectInput("dependent_var", "Choose Dependent Variable",
-                           choices = NULL),
-        shiny::selectInput("independent_var", "Choose Independent Variable",
-                           choices = c("None", "continuous", "categorical")),
-        shiny::numericInput("mean_input", "Enter Mean:", value = 0, step = 0.1,
-                            min = -999, max = 999)
+        shiny::fileInput("file", "Upload a file")
       ),
 
-      # Show a plot of the uploaded data (Histogram for continuous variables)
+      # Show the datatable after submitting a file
       shiny::mainPanel(
-        # Place the table and the rest of the elements in a fluidRow
         shiny::fluidRow(
-          # Place the table in a column that takes up 6 out of 12 units
-          shiny::column(width = 6, DT::dataTableOutput("dataTable")),
-          # Place the histogram plot and other elements in a column that takes up 6 out of 12 units
-          shiny::column(width = 6,
-                        # Display the histogram only when a continuous variable is selected
-                        shiny::conditionalPanel(
-                          condition = "input.independent_var == 'continuous'",
-                          shiny::plotOutput("histPlot")
-                        ),
-                        shiny::verbatimTextOutput("dataView"),  # Display data info
-                        shiny::verbatimTextOutput("dependent_var_info")  # Display dependent variable info
-          )
+          shiny::column(width = 12, DT::dataTableOutput("dataTable"))
         )
       )
     )
