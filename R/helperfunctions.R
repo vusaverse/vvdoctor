51a0ff627ab1bfee59e4990083a00c4928bd2d5f
Corneel den Hartogh <c.f.den.hartogh@vu.nl>
2023-08-14 18:21:54 +0200

    fix: namespaces

--- R/helperfunctions.R
+++ R/helperfunctions.R
@@ -24,11 +24,11 @@ display_data_table <- function(data) {
 #'
 handle_file_upload <- function(input, output, session) {
   # Reactive value to store the uploaded data
-  data <- reactiveVal(NULL)
+  data <- shiny::reactiveVal(NULL)
 
   # Observer for handling file upload
   observeEvent(input$file, {
-    req(input$file)
+    shiny::req(input$file)
 
     # Get the full path of the uploaded file
     full_path <- normalizePath(input$file$datapath)
@@ -37,13 +37,13 @@ handle_file_upload <- function(input, output, session) {
     print(full_path)
 
     # Read the uploaded file
-    data(read.csv(full_path))
+    data(utils::read.csv(full_path))
 
     # Populate the first dropdown with column names
-    updateSelectInput(session, "independent_var", choices = c("", colnames(data())))
-    updateSelectInput(session, "dependent_var", choices = c("", colnames(data())))
+    shiny::updateSelectInput(session, "independent_var", choices = c("", colnames(data())))
+    shiny::updateSelectInput(session, "dependent_var", choices = c("", colnames(data())))
     # Set fileUploaded to TRUE to show the dependent and independent variable dropdowns
-    updateNumericInput(session, "fileUploaded", value = 1)
+    shiny::updateNumericInput(session, "fileUploaded", value = 1)
   })
 
   # Return reactive data for further usage in the app
