87ae8134ed5f181f86192b83b6f4985b3be52dae
tin900 <t.iwan@vu.nl>
2024-04-09 09:05:44 +0200

    feat: call diagnose() to run shiny app

--- R/run_vvdoctor.R
+++ R/run_vvdoctor.R
@@ -13,3 +13,23 @@
 run_vvdoctor <- function() {
   shiny::shinyApp(ui = app_ui(), server = app_server)
 }
+
+
+#' Diagnose Data with Statistical Tests
+#'
+#' This function launches a Shiny application that allows users to upload data
+#' and perform various statistical tests on the dataset. Users can select the
+#' dependent and independent variables, choose the appropriate statistical test,
+#' and view the results.
+#'
+#' @return NULL The function does not return a value, but launches the Shiny app.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(vvdoctor)
+#' diagnose()
+#' }
+diagnose <- function() {
+  shiny::shinyApp(ui = app_ui(), server = app_server)
+}
